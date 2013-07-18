36712c1f3eeb29f2bc6555bc5d69caa9c4e94a51
Sam Jantz <sjantz@riotgames.com>
2013-07-17 22:51:48 -0600

    Initial commit of a basic clojure project using lein

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "This is a basic hello world project to familiarize with clojure."
+  :url "http://github.com/theTechnoWeenie/learning-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main hello-world.core)
