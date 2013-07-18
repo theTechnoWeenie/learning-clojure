9a552a3bb24fc0786871c1a4a644895bcf04a837
Sam Jantz <sjantz@riotgames.com>
2013-07-18 00:01:51 -0600

    Added solution to problem 2, and updated readme to warn about spoilers

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # hello-world
 
-This is a sample project to help myself learn clojure.
+This simple clojure project is a collection of solutions to [Project Euler | projecteuler.net]
+This will spoil the fun (of selected problems) if you go digging through the source.
 
 ## Installation
 
@@ -19,11 +20,12 @@ Currently accepts no options
 
 ## Examples
 
-...
+    $ lein repl
+    $ hello-world.core=> (prob-2 4000000)
 
 ### Bugs
 
-...
+* (prob-2 limit) throws an Arithmeticexception for an intoverflow on the first run, but an additional run with the same parameters succeeds.
 
 ## License
 
--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,8 +1,26 @@
 (ns hello-world.core
   (:gen-class))
 
-(defn hello-world []
+(defn hello-world 
+    "Prints Hello, world! to the console. Returns nil"
+	[]
     (println "Hello, world!"))
+
+;;This is a lazy sequence that defines the fibonacci sequence
+(def fib-seq (lazy-cat [0 1] 
+	(map + fib-seq (rest fib-seq))))
+
+(defn fib-limiter 
+    "Prints all fibonacci numbers that are less than the supplied limit"
+	[limit]
+	(filter #(< % limit) fib-seq))
+
+(defn prob-2 
+    "Algorithm to find the answer to the first problem on projecteuler.net.
+    Given a limit returns the sum of all even fibonacci numbers that are below.
+    "
+	[limit]
+	(reduce + (filter #(and (even? %) (< % limit)) fib-seq)))
 
 (defn -main
   "I don't do a whole lot ... yet."
